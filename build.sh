8ddd4fd45f02b738f8664f843d780af4ca60a37f
Léa Gris <lea.gris@noiraude.net>
2024-11-10 17:54:05 +0100

    imp(build.sh): refactor build script

--- build.sh
+++ build.sh
@@ -1,38 +1,124 @@
+#!/usr/bin/env sh
+
 set -x
 
-PACK_VERSION=$TRAVIS_TAG
+main() {
+  packVersion=${TRAVIS_TAG:-$(git describe --tags)}
 
-[[ -z $PACK_VERSION ]] && exit
+  [ -z "$packVersion" ] && return 1
+  packFormat=${packVersion%%.*}
 
-SUFFIX=-$PACK_VERSION
-DISPLAY_NAME=Invictus$SUFFIX
-RELEASE_FILE=releases/$DISPLAY_NAME.zip
+  baseDir=$(cd "${0%/*}" && pwd)
+  buildDir=$baseDir/build
+  buildResourcesDir=$buildDir/resources
+  archiveDir=$buildDir/archives
+  srcResources=$baseDir/src/main/resources
+  suffix=-$packVersion
+  displayName=Invictus$suffix
+  releaseFile=$archiveDir/$displayName.zip
 
-PROJECTID=220720
+  case "$1" in
+  build) build ;;
+  clean) clean ;;
+  publish) publish ;;
+  *) publish ;;
+  esac
+}
 
-PACK_FORMAT=$(echo $PACK_VERSION | cut -d. -f1)
-# Many ids are red herrings. Weird to have an API without any documentation.
-VERSION_DATA=$(curl -X GET "https://minecraft.curseforge.com/api/game/versions" -H "X-Api-Token: $CURSEFORGE_INVICTUS_TOKEN")
-if [[ $PACK_FORMAT == 1 ]]; then
-  GAME_IDS=$(jq -rc '[.[] | select(((.name | startswith("1.6")) or (.name | startswith("1.7")) or (.name | startswith("1.8"))) and (.gameVersionTypeID==(6, 5, 4))) | .id]' <<< "$VERSION_DATA")
-elif [[ $PACK_FORMAT == 2 ]]; then
-  GAME_IDS=$(jq -rc '[.[] | select(((.name | startswith("1.9")) or (.name | startswith("1.10"))) and (.gameVersionTypeID==(552, 572))) | .id]' <<< "$VERSION_DATA")
-elif [[ $PACK_FORMAT == 3 ]]; then
-  GAME_IDS=$(jq -rc '[.[] | select(((.name | startswith("1.11")) or (.name | startswith("1.12"))) and (.gameVersionTypeID==(599, 628))) | .id]' <<< "$VERSION_DATA")
-elif [[ $PACK_FORMAT == 4 ]]; then
-  GAME_IDS=$(jq -rc '[.[] | select(((.name | startswith("1.13")) or (.name | startswith("1.14"))) and (.gameVersionTypeID==(55023, 64806))) | .id]' <<< "$VERSION_DATA")
-elif [[ $PACK_FORMAT == 5 ]]; then
-  GAME_IDS=$(jq -rc '[.[] | select((.name | startswith("1.15")) and (.gameVersionTypeID==68722)) | .id]' <<< "$VERSION_DATA")
-elif [[ $PACK_FORMAT == 6 ]]; then
-  GAME_IDS=$(jq -rc '[.[] | select((.name | startswith("1.16")) and (.gameVersionTypeID==70886)) | .id]' <<< "$VERSION_DATA")
-elif [[ $PACK_FORMAT == 7 ]]; then
+copyResources() {
+  clean
+  mkdir -p "$buildResourcesDir"
+  cp -r "$srcResources" "$buildResourcesDir" &&
+   cp "$baseDir/LICENSE" "$baseDir/LICENSE_CC_BY_NC_SA.md" "$baseDir/README.md" "$buildResourcesDir"
+}
+
+updatePackMcmeta() {
+  jq --argjson packFormat "$packFormat" \
+  '.pack.pack_format = $packFormat' \
+  "$srcResources/pack.mcmeta" > "$buildResourcesDir/pack.mcmeta"
   exit
-else
-  exit
-fi
+}
 
-mkdir -p releases
-rm -f "$RELEASE_FILE"
-zip -r "$RELEASE_FILE" .  -x "*/\__MACOSX" -x "\.*" -x "build.sh" -x "releases/*"
+zipArchive() {
+  mkdir -p "$archiveDir"
+  rm -f "$releaseFile"
+  case "$-" in *x*) q= ;; *) q=-q ;; esac
+  cd "$buildResourcesDir" || return 1
+  zip ${q:+"$q"} -r9 "$releaseFile" . -x '__MACOSX' -x '\.*'
+  rc=$?
+  cd "$baseDir" || return 1
+  zip ${q:+"$q"} -9 "$releaseFile" LICENSE LICENSE_CC_BY_NC_SA.md README.md
+  return $((rc || $?))
+}
 
-curl -X POST -H "X-Api-Token: $CURSEFORGE_INVICTUS_TOKEN" -F metadata="{\"changelog\":\"https://github.com/InvictusGraphics/Invictus_Textures/commits/master\",\"changelogType\":\"text\",\"displayName\":\"$DISPLAY_NAME\",\"gameVersions\":$GAME_IDS,\"releaseType\":\"release\"}" -F file=@"$RELEASE_FILE" "https://minecraft.curseforge.com/api/projects/$PROJECTID/upload-file"
+build() {
+  copyResources
+  updatePackMcmeta
+  zipArchive
+}
+
+clean() {
+  rm -fr "${baseDir:?}/build"
+}
+
+publish() {
+  if build && [ -n "$CURSEFORGE_INVICTUS_TOKEN" ]; then
+
+    projectId=220720
+
+    # Many ids are red herrings. Weird to have an API without any documentation.
+    versionData=$(CurseForge_gameVersions) || return 1
+    #versionData='[{"gameVersionTypeID":70886,"id":42,"slug":"string","name":"1.16"}]'
+
+    # shellcheck disable=SC2016 # Constant expression with $ signs
+    case "$packFormat" in
+    1) jqs='[.[] | select(((.name | startswith("1.6")) or (.name | startswith("1.7")) or (.name | startswith("1.8"))) and (.gameVersionTypeID==(6, 5, 4))) | .id]' ;;
+    2) jqs='[.[] | select(((.name | startswith("1.9")) or (.name | startswith("1.10"))) and (.gameVersionTypeID==(552, 572))) | .id]' ;;
+    3) jqs='[.[] | select(((.name | startswith("1.11")) or (.name | startswith("1.12"))) and (.gameVersionTypeID==(599, 628))) | .id]' ;;
+    4) jqs='[.[] | select(((.name | startswith("1.13")) or (.name | startswith("1.14"))) and (.gameVersionTypeID==(55023, 64806))) | .id]' ;;
+    5) jqs='[.[] | select((.name | startswith("1.15")) and (.gameVersionTypeID==68722)) | .id]' ;;
+    6) jqs='[.[] | select((.name | startswith("1.16")) and (.gameVersionTypeID==70886)) | .id]' ;;
+    *) return 1 ;;
+    esac
+
+    GAME_IDS=$(jq -cn --argjson versionData "$versionData" "\$versionData | $jqs") || return 1
+
+    case "$-" in *x*)
+      jq -n \
+        --arg changelog 'https://github.com/InvictusGraphics/Invictus_Textures/commits/master' \
+        --arg displayName "$displayName" \
+        --argjson gameVersions "$GAME_IDS" \
+        '{"changelog":$changelog,"changelogType":"text","displayName":$displayName,"gameVersions":$gameVersions,"releaseType":"release"}' >&2
+      ;;
+    esac
+
+    jq -c -n \
+      --arg changelog 'https://github.com/InvictusGraphics/Invictus_Textures/commits/master' \
+      --arg displayName "$displayName" \
+      --argjson gameVersions "$GAME_IDS" \
+      '{"changelog":$changelog,"changelogType":"text","displayName":$displayName,"gameVersions":$gameVersions,"releaseType":"release"}' |
+      CurseForge_uploadFile "$projectId" "$releaseFile"
+  fi
+}
+
+## CurseForge API
+CURSE_FORGE_API=https://minecraft.curseforge.com/api
+CURSE_FORGE_API_TOKEN=${CURSEFORGE_INVICTUS_TOKEN}
+
+# Uploads a file to CurseForge API
+# @params
+# $1: The project ID
+# $2: The path to the file to upload
+# @streams
+# <: the JSON metadata
+CurseForge_uploadFile() {
+  curl -X POST -H "X-Api-Token: ${CURSE_FORGE_API_TOKEN:?}" -F metadata=@- \
+    -F "file=@${2:?}" "$CURSE_FORGE_API/projects/${1:?}/upload-file"
+}
+
+# Retrieves game versions
+CurseForge_gameVersions() {
+  curl -X GET "$CURSE_FORGE_API/game/versions" -H "X-Api-Token: ${CURSE_FORGE_API_TOKEN:?}"
+}
+
+main "$@"
